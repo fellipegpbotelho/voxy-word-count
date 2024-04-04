ff39b7e5700bce0d44c78dc0e54c69e3db8acda5
Fellipe Botelho <fellipegpbotelho@gmail.com>
2024-04-04 15:29:23 -0300

    feat: add custom error handling

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from fastapi.exception_handlers import (
+    http_exception_handler,
+    request_validation_exception_handler,
+)
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.exceptions import HTTPException as StarletteHTTPException
 from starlette.responses import FileResponse
 
 from models import WordCountPayload
@@ -16,11 +22,23 @@ app.add_middleware(
 )
 
 
+@app.exception_handler(StarletteHTTPException)
+async def custom_http_exception_handler(request, exc):
+    print(f"[http_error]: {repr(exc)}")
+    return await http_exception_handler(request, exc)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    print(f"[client_error]: {exc}")
+    return await request_validation_exception_handler(request, exc)
+
+
 @app.get("/")
-def index():
+async def index():
     return FileResponse("index.html")
 
 
 @app.post("/words/count")
-def count_words(payload: WordCountPayload):
+async def count_words(payload: WordCountPayload):
     return {"count": len(payload.text.rsplit())}
