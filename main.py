15082a37a03c926feb8f5ef1768a27905740e437
Fellipe Botelho <fellipegpbotelho@gmail.com>
2024-04-04 15:00:34 -0300

    feat: deal with cors

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import FileResponse
 
+from models import WordCountPayload
 
 app = FastAPI()
 
-
-class Payload(BaseModel):
-    text: str
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -16,5 +22,5 @@ def index():
 
 
 @app.post("/words/count")
-def count_words(payload: Payload):
+def count_words(payload: WordCountPayload):
     return {"count": len(payload.text.rsplit())}
